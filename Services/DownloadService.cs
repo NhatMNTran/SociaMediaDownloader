782368e28eb3bd56a12dd77d4306f9f95017b839
NhatMNT <trannhat100@gmail.com>
2026-05-16 11:09:02 -0700

    Created DownloadService.cs to inititate Download action for Youtube

---
+++ Services/DownloadService.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SocialMediaDownloader.Services
+{
+    public class DownloadService
+    {
+        private readonly string ytDlpPath =
+            Path.Combine("Assets", "yt-dlp.exe");
+
+        public async Task Download(
+            string url,
+            string destination,
+            string formatId)
+        {
+            ProcessStartInfo psi = new()
+            {
+                FileName = ytDlpPath,
+                Arguments =
+                    $"-f {formatId} -o \"{destination}\\%(title)s.%(ext)s\" {url}",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using Process process = Process.Start(psi);
+
+            await process.WaitForExitAsync();
+        }
+    }
+}
