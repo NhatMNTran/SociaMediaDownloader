0b47eeca11269c150895dcbd98945d29afe8bfa3
NhatMNT <trannhat100@gmail.com>
2026-05-16 10:58:07 -0700

    Created MediaInfo for formatting files

---
+++ Models/MediaInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SocialMediaDownloader.Models
+{
+    public class MediaInfo
+    {
+        public string Title { get; set; }
+
+        public string Platform { get; set; }
+
+        public List<DownloadOption> Formats { get; set; }
+    }
+}
