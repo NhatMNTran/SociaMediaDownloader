280473e1da66bf448607f3da4654e8fc88b3866d
NhatMNT <trannhat100@gmail.com>
2026-05-16 10:57:15 -0700

    Created DownloadOption.cs for formatting media files

---
+++ Models/DownloadOption.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialMediaDownloader.Models
+{
+    public class DownloadOption
+    {
+        public string FormatId { get; set; }
+
+        public string Extension { get; set; }
+
+        public string Resolution { get; set; }
+
+        public bool AudioOnly { get; set; }
+
+        public string DisplayText =>
+            AudioOnly
+                ? $"{Extension.ToUpper()} Audio"
+                : $"{Extension.ToUpper()} {Resolution}";
+    }
+}
