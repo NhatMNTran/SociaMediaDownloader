55b2de5dd04a748dc7c12fcf1754842f4c0ebb63
NhatMNT <trannhat100@gmail.com>
2026-05-16 11:14:26 -0700

    Create FileDialogService to communicate with Windows Folders

---
+++ Services/FileDialogService.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace SocialMediaDownloader.Services
+{
+    public class FileDialogService
+    {
+        public string SelectFolder()
+        {
+            using FolderBrowserDialog dialog = new();
+
+            return dialog.ShowDialog() ==
+                   DialogResult.OK
+                ? dialog.SelectedPath
+                : string.Empty;
+        }
+    }
+}
