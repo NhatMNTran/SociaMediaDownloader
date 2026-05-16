3c02530ebd87bf6c214d74434b15d2544d75f9c6
NhatMNT <trannhat100@gmail.com>
2026-05-16 11:01:58 -0700

    Created RelayCommand.cs to handle execute events

---
+++ Helpers/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SocialMediaDownloader.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
